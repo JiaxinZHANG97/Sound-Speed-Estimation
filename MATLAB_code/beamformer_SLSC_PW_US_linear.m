b7fe33e6946436fe506e63956463e212d78f9f9b
JiaxinZHANG97 <635622125@qq.com>
2024-06-28 13:03:28 -0400

    Add zero_out_flag

--- MATLAB_code/beamformer_SLSC_PW_US_linear.m
+++ MATLAB_code/beamformer_SLSC_PW_US_linear.m
@@ -21,7 +21,7 @@
 % Version1: Jiaxin Zhang (jzhan295@jhu.edu)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [slsc,cc,metadata,x_axis,z_axis] = beamformer_SLSC_PW_US_linear(delay_data,metadata)
+function [slsc,cc,metadata,x_axis,z_axis] = beamformer_SLSC_PW_US_linear(delay_data,metadata,zero_out_flag)
 %% Loading SLSC parameters
 if ~isfield(metadata,'US')
     metadata.US.SLSC.k_factor=1; 
@@ -42,10 +42,11 @@ k = metadata.US.SLSC.k_factor*metadata.Sys.fs/metadata.Sys.f0;
 k = round(k)+1-mod(round(k),2); % Round to odd
 metadata.US.SLSC.k_size=k;
 maxM=metadata.US.SLSC.maxM;
+cc = zeros(size(delay_data,1),maxM,size(delay_data,3));
 slsc = zeros(size(delay_data,1),size(delay_data,3),maxM);
 
 %% Modified for multiple plane wave
-cc = zeros(size(delay_data,1),maxM,size(delay_data,3),metadata.Sys.N_pw);
+% cc_pw = zeros(size(delay_data,1),maxM,size(delay_data,3),metadata.Sys.N_pw);
 for pw = 1:metadata.Sys.N_pw
     fprintf(['Plane wave: ',num2str(pw),'\n'])
     %% Remove DC along the axial dimension
@@ -64,17 +65,22 @@ for pw = 1:metadata.Sys.N_pw
         cc2 = slsc_mex(temp,lag,k,1);
         cc2(isnan(cc2)) = 0;
         cc_temp(:,:,ch) = cc2;
-        cc(:,:,ch,pw) = cc2;
+        % cc_pw(:,:,ch,pw) = cc2;
     end
+    cc = cc + cc_temp;
 
     %% Sum along lags
     slsc_temp = zeros(size(delay_data,1),size(delay_data,3),maxM);
     for M=1:maxM
         slsc_temp(:,:,M) = squeeze(sum(cc_temp(:,1:M,:),2))/M;
     end
-    slsc_temp(slsc_temp<0) = 0; % set negative SLSC image pixels to zero (Nair AA: Robust Short-Lag Spatial Coherence Imaging)
+    if zero_out_flag
+        slsc_temp(slsc_temp<0) = 0; % set negative SLSC image pixels to zero (Nair AA: Robust Short-Lag Spatial Coherence Imaging)
+    end
     slsc = slsc + slsc_temp;
 end
+cc = cc / metadata.Sys.N_pw;
+slsc = slsc / metadata.Sys.N_pw;
 
 %% Axis generation
 x_axis = linspace(metadata.Sys.extent(1),metadata.Sys.extent(2),metadata.Sys.N_ch); % [mm]
